85641eddae3d0995d41c6d9a523c307ec177378f
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-03-30 17:38:10 +0100

    title added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,29 @@
-import { Grid, Row, Link, Card, Badge, Tooltip, Button } from "@nextui-org/react"
+import { Grid, Row, Link, Card, Badge, Tooltip, Button, Head } from "@nextui-org/react"
 import { BsArrowReturnRight } from 'react-icons/bs'
 import Router from 'next/router'
 
 export default function home() {
   
   return (
-    <div>
-      <Grid.Container css={{ display: "flex" }}>
-        <Grid css={{ marginLeft: "auto", marginRight: "auto", width: "45%" }}>
-          <Card isPressable={true} variant="flat" css={{ marginTop: "10%", borderRadius: "10px" }} onPress={() => Router.push('/posts/3DFMRJVv5QnwPCTi')}>
-            <Row>
-                <h1 style={{ fontSize: "25px", fontWeight: "400", marginTop: "5%", marginLeft: "5%" }}>Bitcoin Clipper Malware Investigation</h1> 
-                <h1 style={{ fontSize: "18px", fontWeight: "400", marginTop: "5%", marginLeft: "38%", color: "#767b84" }}>Mar 29, 2023</h1>
-            </Row>
-            <p style={{ fontSize: "18px", fontWeight: "400", color: "#ced2d8", marginLeft: "5%" }}>An in-depth Investigation into a new form of undetectable Malware. Figure out how it works how it works and how</p>
-            <p style={{ fontSize: "18px", fontWeight: "400", color: "#ced2d8", marginLeft: "5%" }}>you can prevent getting infected by it. Learn how to create your own version for research purposes or just to</p>
-            <p style={{ fontSize: "18px", fontWeight: "400", color: "#ced2d8", marginLeft: "5%", marginBottom: "5%" }}>get a feel for how the Malware functions.</p>
-          </Card>
-        </Grid>
-      </Grid.Container>
-    </div>
+    <>
+    <Head>
+      <title>katsu.bio</title>
+    </Head>
+      <div>
+        <Grid.Container css={{ display: "flex" }}>
+          <Grid css={{ marginLeft: "auto", marginRight: "auto", width: "45%" }}>
+            <Card isPressable={true} variant="flat" css={{ marginTop: "10%", borderRadius: "10px" }} onPress={() => Router.push('/posts/3DFMRJVv5QnwPCTi')}>
+              <Row>
+                  <h1 style={{ fontSize: "25px", fontWeight: "400", marginTop: "5%", marginLeft: "5%" }}>Bitcoin Clipper Malware Investigation</h1> 
+                  <h1 style={{ fontSize: "18px", fontWeight: "400", marginTop: "5%", marginLeft: "38%", color: "#767b84" }}>Mar 29, 2023</h1>
+              </Row>
+              <p style={{ fontSize: "18px", fontWeight: "400", color: "#ced2d8", marginLeft: "5%" }}>An in-depth Investigation into a new form of undetectable Malware. Figure out how it works how it works and how</p>
+              <p style={{ fontSize: "18px", fontWeight: "400", color: "#ced2d8", marginLeft: "5%" }}>you can prevent getting infected by it. Learn how to create your own version for research purposes or just to</p>
+              <p style={{ fontSize: "18px", fontWeight: "400", color: "#ced2d8", marginLeft: "5%", marginBottom: "5%" }}>get a feel for how the Malware functions.</p>
+            </Card>
+          </Grid>
+        </Grid.Container>
+      </div>    
+    </>
   )
 }
