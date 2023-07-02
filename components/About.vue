fa841734e87239ac0639660067c337f761083718
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-02 14:35:09 +0100

    Update files

---
+++ components/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="max-w-xl">
+    <h1 class="mt-4 text-3xl font-bold flex md:items-end md:flex-row flex-col">
+      <div>Katsu</div>
+      <div class="flex md:ml-3">
+        <div class="text-lg">[he/him]</div>
+        <div class="text-xl ml-3">
+          <a
+            href="https://discord.com/channels/@me/1052982721598738522/"
+            target="_blank"
+            class="hover:opacity-80 transition"
+            ><font-awesome-icon :icon="['fab', 'discord']"
+          /></a>
+          <a
+            href="https://github.com/NotKatsu/"
+            target="_blank"
+            class="hover:opacity-80 transition"
+            ><font-awesome-icon :icon="['fab', 'github']"
+          /></a>
+        </div>
+      </div>
+    </h1>
+
+    <img
+      src="https://skillicons.dev/icons?i=py,go,html,css,fastapi,mongodb,mysql,docker,vim,neovim,vscode,git"
+      class="mt-4"
+    />
+  </div>
+</template>
---
+++ components/Project.vue
@@ -0,0 +1,52 @@
+<template>
+  <a
+    :href="link"
+    target="_blank"
+    class="flex-1 bg-opacity-30 p-4 my-2 rounded-md hover:scale-105 transform ease-in-out duration-300"
+    :class="color"
+  >
+    <div class="ml-4 flex flex-col justify-center">
+      <h2 class="text-lg font-bold flex items-center">
+        {{ name }}
+        <span v-if="year" class="text-sm font-normal ml-4 hidden sm:block"
+          >[{{ year }}]</span
+        >
+      </h2>
+      <p class="text-sm">
+        {{ description }}
+      </p>
+    </div>
+
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    icon: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      default: '',
+    },
+    link: {
+      type: String,
+      default: '',
+    },
+    year: {
+      type: String,
+      default: null,
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+    color: {
+      type: String,
+      default: 'bg-white',
+    },
+  },
+}
+</script>
