bf8a4db7c5468d74afdd61efa5edde65e3cd70d7
NotKatsu <122122838+NotKatsu@users.noreply.github.com>
2023-07-02 14:37:09 +0100

    Add old code back to dir

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="max-w-3xl px-8 mt-7 mx-auto">
+      <div>
+        <img
+          src="~/assets/img/avatar.png"
+          alt="Anime Boy with yellow hat"
+          class="w-32 h-32 rounded-3xl inline avatar"
+        />
+      </div>
+      <About />
+      <Divider />
+      <Projects />
+      <Divider />
+      <Contact />
+      <Divider />
+
+      <div class="copyright text-center mb-8">
+        Made with ❤️ by Katsu
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
+
+Donations
